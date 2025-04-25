05e0bb12510a265bfbb1647b074253d3363160ff
Bhoopendra Singh <thakurbhoopendrasingh4422@gmail.com>
2025-04-25 06:16:31 +0530

    backend

---
+++ Schema.js
@@ -0,0 +1,23 @@
+const mongoose=require('mongoose');
+const { number } = require('yup');
+const registerSchema = new mongoose.Schema({
+    name:String,
+    email:String,
+    phoneNo:String,
+    password:String,
+    history: [
+      {
+        response: mongoose.Schema.Types.Mixed, // to store the full GPT response
+        createdAt: { type: Date, default: Date.now },
+      },
+    ],
+    credits:{
+      type:Number,
+      default:2
+    }
+  
+})
+
+
+module.exports = 
+    mongoose.model("User", registerSchema);
